a59560238c609e759a61dc4002f0a5bb5700eb6f
ExecutiveDave <davidwilliams.ca68@icloud.com>
2024-11-14 04:33:14 +0000

    added getElementById

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-// Write your JavaScipt code below this line. 
+// Write your JavaScipt code below this line.
+const mainTitle = document.getElementById('main-title')
+mainTitle.textContent = "Cat Adoption Center"
+
+const description = document.getElementById("description")
+description.textContent = "Find your Cat"
+
+const cat1Button = document.getElementById('cat-1-button')
+cat1Button.addEventListener('click', () => {
+  console.log('clicked the first cat')
+
+})
